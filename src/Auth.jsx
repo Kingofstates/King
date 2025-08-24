6322992e52cd28f26e9a6d52823ffc413bfbe396
King of states <hackdemo18@gmail.com>
2025-08-24 12:39:59 +0000

    tes

--- src/Auth.jsx
+++ src/Auth.jsx
@@ -33,13 +33,8 @@ export default function Auth({ onLogin }) {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 px-4 ">
-<<<<<<< HEAD
-      <div className="bg-white p-6 rounded-lg shadow-md  sm-w-400px md-w-768px lg-w-100px  max-w-md w-300px">
-        <h2 className="text-2xl font-semibold mb-4 text-center text-red-400">
-=======
       <div className="bg-white p-6 rounded-lg shadow-md w-full max-w-md w-300px">
-        <h2 className="text-4xl font-semibold mb-4 text-center ">
->>>>>>> bdd58ffec3e90277795ddbf6ad1159850fb5f354
+        <h2 className="text-3xl font-semibold mb-4 text-center text-red-400">
           {isSignup ? 'Sign Up' : 'Login'}
         </h2>
         <form onSubmit={handleSubmit} className="space-y-4">
